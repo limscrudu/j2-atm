bf8f43543c21a7c4a409c92e46e150696519be02
limscrudu <limscrudu@gmail.com>
2018-03-15 17:21:37 +0000

    initial empty spring boot project

---
+++ j2-atm/src/main/java/com/limscrudu/j2atm/J2AtmApplication.java
@@ -0,0 +1,12 @@
+package com.limscrudu.j2atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2AtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2AtmApplication.class, args);
+	}
+}
